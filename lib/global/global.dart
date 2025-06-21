df6bc2bac7f6f3fa9f82704762e7755c5b6ea409
KasunMendis <tkomendis@gmail.com>
2025-06-21 20:27:37 +0530

    added register, login and splash screen

---
+++ lib/global/global.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import '../models/user_model.dart';
+
+final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+User? currentUser;
+UserModel? userModalCurrentInfo;
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
-import 'package:fixme/screens/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fixme/screens/login_screen.dart';
+import 'package:fixme/splashScreen/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:fixme/screens/home_page.dart';
 import 'package:fixme/screens/register_screen.dart';
 import 'package:fixme/themeProvider/theme_provider.dart';
-import 'package:flutter/material.dart';
 
-void main() {
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,11 +20,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FixMe',
       themeMode: ThemeMode.system,
       theme: MyThemes.lightTheme,
       darkTheme: MyThemes.darkTheme,
-      home: RegisterScreen(),
+      home: SplashScreen(),
     );
   }
 }
