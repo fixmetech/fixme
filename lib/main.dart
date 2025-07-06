acf42c3a8f15a5e4469449044a992ea4e36d20f0
ShaminAkarsha <shaminakarsha@gmail.com>
2025-07-06 18:31:14 +0530

    add firebase cli and config files

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fixme/app.dart';
+import 'package:fixme/firebase_options.dart';
+import 'package:fixme/global/global.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(); 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  ).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
   runApp(const MyApp());
 }
 
