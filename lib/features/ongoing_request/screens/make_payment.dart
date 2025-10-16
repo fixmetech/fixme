5af19e5149619266443d3f488f9844950a41ee74
KasunMendis <tkomendis@gmail.com>
2025-10-16 15:53:36 +0530

    the ratings and reviews are saved in the technicianFeedback collection

--- lib/features/ongoing_request/screens/make_payment.dart
+++ lib/features/ongoing_request/screens/make_payment.dart
@@ -1,4 +1,4 @@
-import 'package:fixme/features/ongoing_request/completed_job.dart';
+import 'package:fixme/features/ongoing_request/screens/completed_job.dart';
 import 'package:fixme/services/stripe_service.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
